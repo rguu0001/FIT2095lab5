e85adee39c1305a94bbcade1be10465e4aafc8b4
rguu0001 <rguu0001@student.monash.edu>
2019-08-29 00:58:16 +1000

    hello

---
+++ app.js
@@ -0,0 +1,43 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+let db=[];
+// let ejs=require('ejs');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.static("public/images"));
+app.use(express.static("public/styles"));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.get("/", (req, res) => {
+    res.render("index.html");
+})
+
+app.get("/addtask.html", (req, res) => {
+    res.render("addtask.html");
+})
+
+app.get("/alltasks.html", (req, res) => {
+    res.render("alltasks.html", { task: db});
+})
+
+app.post("/newTask", (req, res) => {
+    console.log(req.body);
+    // console.log(req.body.taskName);
+    
+    db.push(req.body);
+    console.log(db);
+    
+    res.render("addtask.html", { task: db});
+});
+
+app.listen(8080);
+
+
+
+
